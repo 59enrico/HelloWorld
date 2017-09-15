b46854f6bcc7acd3785a3879e7684f42c34c7358
59enrico <enileue@gmail.com>
2017-09-15 13:02:30 +0200

    added wait time between 2 outprints;20170915_13:02

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,8 +4,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
-		
-		
+		try{Thread.sleep(5000);}catch(InterruptedException e) {}; //5000 mil.sec. Pause da 1000 zu kurz 
+		System.out.println("I waited 5 seconds to say this.");
 	}
 
 }
