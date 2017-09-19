c84770fd00f9b73c228400724c9f656d650cb68f
59enrico <enileue@gmail.com>
2017-09-19 21:44:50 +0200

    Comment change

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
-		try{Thread.sleep(5000);}catch(InterruptedException e) {}; //5000 mil.sec. Pause da 1000 zu kurz 
+		try{Thread.sleep(5000);}catch(InterruptedException e) {}; //5000 mil.sec. Pause
 		System.out.println("I waited 5 seconds to say this.");
 	}
 
